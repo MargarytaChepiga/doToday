a655e5d4529851a6c6184be1e3f0cf35cd6dd140
MargartyaChepiga <chepigamargaryta@gmail.com>
2017-04-02 18:42:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dotoday/RecyclerViewAdapter.java
@@ -0,0 +1,30 @@
+package com.example.dotoday;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.List;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolders> {
+    private List<myTask> task;
+    protected Context context;
+    public RecyclerViewAdapter(Context context, List<myTask> task) {
+        this.task = task;
+        this.context = context;
+    }
+    @Override
+    public RecyclerViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerViewHolders viewHolder = null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.to_do_list, parent, false);
+        viewHolder = new RecyclerViewHolders(layoutView, (List)task);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(RecyclerViewHolders holder, int position) {
+        holder.categoryTitle.setText(task.get(position).getTask());
+    }
+    @Override
+    public int getItemCount() {
+        return this.task.size();
+    }
+}
---
+++ app/src/main/java/com/example/dotoday/myTask.java
@@ -0,0 +1,12 @@
+package com.example.dotoday;
+
+public class myTask {
+    private String task;
+    public myTask() {}
+    public myTask(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
